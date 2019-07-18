e850a58ec03c0b3eea54008ce24252449abfc059
flywithbug <flywithbug@gmail.com>
2019-07-18 17:24:25 +0800

    mgo index

---
+++ index.go
@@ -0,0 +1,32 @@
+package mongo
+
+import "gopkg.in/mgo.v2"
+
+type Index struct {
+	Collection  string
+	DBName  string
+	Index  mgo.Index
+	DropIndex  []string
+}
+
+func MgoIndex(list []Index,forceSync bool)error  {
+	for _, v := range list{
+		_,c := connect(v.DBName,v.Collection)
+		if len(v.DropIndex) > 0 {
+			for _, dv := range v.DropIndex {
+				if err := c.DropIndexName(dv);err != nil {
+					return err
+				}
+			}
+			if forceSync {
+				if err := c.DropIndexName(v.Index.Name);err != nil {
+					return err
+				}
+			}
+			if err := c.EnsureIndex(v.Index);err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
